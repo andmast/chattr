a14d3f72508dd4cd8418885b09c1851d9bac74d5
Andrea <anmastrantoni@gmail.com>
2019-05-06 22:17:38 +0000

    React compents made

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,59 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello Andrea there :)</h1>
+    <div>
+      <NavBar/>
+      <Main />
+      <Footer/>
+    </div>
+    );
+  }
+}
+
+class NavBar extends Component {
+  render (){
+    return (
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">
+          Chatty
+        </a>
+      </nav>
+    );
+  }
+
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="chatbar">
+        <input
+          clasName="chatbar-username"
+          placeholder="Your Name (Optional)"
+        />
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+        />
+      </footer>
+    );
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <main className="messages">
+        <div className="message">
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content">
+            I won't be impressed with technology until I can download food.
+          </span>
+        </div>
+        <div className="message system">
+          Anonymous1 changed their name to nomnom.
+        </div>
+      </main>
     );
   }
 }
