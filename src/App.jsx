1fe7b1530d98124c3be08a5cd0c40bc320e6c592
Andrea <anmastrantoni@gmail.com>
2019-05-06 21:51:41 +0000

    Added CSS to Home.Scss added the script tags and the div react root

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Hello Andrea :)</h1>
+      <h1>Hello Andrea there :)</h1>
     );
   }
 }
